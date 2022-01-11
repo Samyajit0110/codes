f3af1a050779c5a7f2bc9e908366b403816ace60
Sambit Sarkar <88318404+Sscuber03@users.noreply.github.com>
2022-01-11 23:12:26 +0530

    just trying out the pull request thing

--- CCodes/fibonacciN.c
+++ CCodes/fibonacciN.c
@@ -2,10 +2,10 @@
 void main()
 {
     int a = 0,b = 1,sum = 0, n;
-    printf("Enter n\n");
+    printf("Enter the value of n\n");
     scanf("%d",&n);
     printf("%d %d ", a,b);
-    for(int i = 1; i <= n - 2; i++)
+    for(int i = 1; i < n - 1; i++)
     {
         sum = a + b;
         a = b;
